082fbfdebd63943eb75d29a59e48916cd78e46b7
Sébastien Eustace <sebastien@eustace.io>
2022-08-20 16:56:52 +0200

    Add a wheel builder

--- tests/installation/test_chef.py
+++ tests/installation/test_chef.py
@@ -9,6 +9,7 @@ from packaging.tags import Tag
 from poetry.core.packages.utils.link import Link
 
 from poetry.installation.chef import Chef
+from poetry.utils.env import EnvManager
 from poetry.utils.env import MockEnv
 
 
@@ -103,3 +104,47 @@ def test_get_cache_directory_for_link(config: Config, config_cache_dir: Path):
     )
 
     assert directory == expected
+
+
+def test_prepare_sdist(config: Config, config_cache_dir: Path) -> None:
+    chef = Chef(config, EnvManager.get_system_env())
+
+    archive = (
+        Path(__file__)
+        .parent.parent.joinpath("fixtures/distributions/demo-0.1.0.tar.gz")
+        .resolve()
+    )
+
+    destination = chef.get_cache_directory_for_link(Link(archive.as_uri()))
+
+    wheel = chef.prepare(archive)
+
+    assert wheel.parent == destination
+    assert wheel.name == "demo-0.1.0-py3-none-any.whl"
+
+
+def test_prepare_directory(config: Config, config_cache_dir: Path):
+    chef = Chef(config, EnvManager.get_system_env())
+
+    archive = Path(__file__).parent.parent.joinpath("fixtures/simple_project").resolve()
+
+    wheel = chef.prepare(archive)
+
+    assert wheel.name == "simple_project-1.2.3-py2.py3-none-any.whl"
+
+
+def test_prepare_directory_with_extensions(
+    config: Config, config_cache_dir: Path
+) -> None:
+    env = EnvManager.get_system_env()
+    chef = Chef(config, env)
+
+    archive = (
+        Path(__file__)
+        .parent.parent.joinpath("fixtures/extended_with_no_setup")
+        .resolve()
+    )
+
+    wheel = chef.prepare(archive)
+
+    assert wheel.name == f"extended-0.1-{env.supported_tags[0]}.whl"
